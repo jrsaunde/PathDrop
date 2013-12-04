2303c4df08d9666bcc9ff2ab3b1a84004cfa7d84
hliu9@ncsu.edu <Hank@Colossus>
2013-12-03 22:17:45 -0500

    passing code to jamie

--- src/guiFX/ConsoleTest.java
+++ src/guiFX/ConsoleTest.java
@@ -25,4 +25,13 @@ public class ConsoleTest extends Application {
 FontRenderContext frc = new FontRenderContext(affinetransform,true,true);     
 Font font = new Font("Tahoma", Font.PLAIN, 12);
 int textwidth = (int)(font.getStringBounds(text, frc).getWidth());
-int textheight = (int)(font.getStringBounds(text, frc).getHeight());*/
+int textheight = (int)(font.getStringBounds(text, frc).getHeight());
+
+ myComponent.setStyle("-fx-text-fill: white;"+
+    "-fx-background-color: black;"+
+    "-fx-font: Courier New;"+
+    "-fx-font-family: Courier New;"+
+    "-fx-font-weight: bold;"+
+    "-fx-font-size: 30;");
+    
+   */
